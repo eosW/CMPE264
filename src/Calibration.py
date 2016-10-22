165582455fcb616ffe14e0198f88f0373bcad78a
endofsamsara <end.wrm@gmail.com>
2016-10-22 15:07:07 -0700

    add interface

---
+++ src/Calibration.py
@@ -0,0 +1,16 @@
+
+
+class Calibration:
+    def __init__(self):
+        """
+        load the pictures
+        """
+        pass
+
+    def get_true_b(self, colors):
+        """
+        get the linear B
+        :param colors: input B of three colors in sequence of BGR, uint8
+        :return: linear B in BGR, float
+        """
+        pass
