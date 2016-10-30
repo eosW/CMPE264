0c9725bb68a966aa50233d75bf311070eb6a7ffd
troyswiftie <troyswiftie@hotmail.com>
2016-10-29 20:04:50 -0700

    Update stack.py

--- src/stack.py
+++ src/stack.py
@@ -1,9 +1,11 @@
 import cv2
+from Calibration import Calibration
 import numpy as np
-
 img0 = cv2.imread("180.jpg",0)
 img1 = cv2.imread("20.jpg",0)
 img2 = cv2.imread("6.jpg",0)
+
+c = Calibration()
 
 def linearize0():
     for i in range(img0.height):
@@ -12,9 +14,7 @@ def linearize0():
             b=img0[i,j,0]
             g=img0[i,j,1]
             r=img0[i,j,2]
-            img0[i,j]=get_true_b(b,g,r)
-
-    cv2.imwrite("lin0.jpg", img0)
+            img0[i,j]= c.get_true_b(b,g,r)
 
 def linearize1():
     for i in range(img1.height):
@@ -23,9 +23,7 @@ def linearize1():
             b=img1[i,j,0]
             g=img1[i,j,1]
             r=img1[i,j,2]
-            img1[i,j]=get_true_b(b,g,r)
-
-    cv2.imwrite("lin1.jpg", img1)
+            img1[i,j]=c.get_true_b(b,g,r)
 
 def linearize2():
     for i in range(img2.height):
@@ -34,13 +32,5 @@ def linearize2():
             b=img2[i,j,0]
             g=img2[i,j,1]
             r=img2[i,j,2]
-            img2[i,j]=get_true_b(b,g,r)
-
-    cv2.imwrite("lin2.jpg", img2)
-
-  
-  
-
-
-
+            img2[i,j]=c.get_true_b(b,g,r)
 
